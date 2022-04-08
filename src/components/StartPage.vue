c69c34fb5eb3e0a700e0d52b41575f6cc5ee583b
Aleksandr Seryapin <al_seryapin@mail.ru>
2022-04-08 16:35:09 +0300

    feat: add beer table

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,8 +1,52 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const beer = reactive([]);
+
+async function getBeer() {
+  try {
+    const request = await fetch(
+      "https://random-data-api.com/api/beer/random_beer"
+    );
+    if (request.ok) {
+      beer.push(await request.json());
+    }
+    console.log("beer", beer);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+}
+</script>
 
 <template>
-  <div class="start-page__wrapper">
-    <button class="advise-btn">Advise</button>
+  <div v-if="beer.length <= 0" class="start-page__wrapper">
+    <button @click="getBeer" class="advise-btn">Advise</button>
+  </div>
+  <div v-else class="beer__wrapper">
+    <div class="beer__alc">Alc: {{ beer[beer.length - 1].alcohol }}</div>
+    <div class="beer__title">"{{ beer[beer.length - 1].name }}"</div>
+    <div class="beer__sub-title">{{ beer[beer.length - 1].brand }}</div>
+
+    <div class="beer__item">
+      <span class="beer__item_legend">Style: </span
+      >{{ beer[beer.length - 1].style }}
+    </div>
+    <div class="beer__item">
+      <span class="beer__item_legend">Bitterness: </span
+      >{{ beer[beer.length - 1].blg }}
+    </div>
+    <div class="beer__item">
+      <span class="beer__item_legend">Hop variety: </span
+      >{{ beer[beer.length - 1].hop }}
+    </div>
+    <div class="beer__item">
+      <span class="beer__item_legend">Yeast: </span
+      >{{ beer[beer.length - 1].yeast }}
+    </div>
+    <div class="beer__item">
+      <span class="beer__item_legend">Malts: </span>
+      {{ beer[beer.length - 1].malts }}
+    </div>
   </div>
 </template>
 
@@ -65,5 +109,53 @@
     left: -12px;
     opacity: 0;
   }
+}
+
+.beer__wrapper {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  min-width: max-content;
+  max-width: 400px;
+  border: 3px #ccc solid;
+  border-radius: 15px;
+}
+
+.beer__title {
+  color: var(--color-beer-pale-gold);
+  font-size: 3rem;
+  align-self: center;
+}
+
+.beer__sub-title {
+  font-size: 1.5rem;
+  font-style: italic;
+  align-self: center;
+  margin-bottom: 2rem;
+}
+
+.beer__item {
+  font-size: 1.2rem;
+}
+
+.beer__item::after {
+  content: "";
+  display: block;
+  height: 2px;
+  background-color: rgba(204, 204, 204, 0.6);
+}
+.beer__item_legend {
+  font-weight: bold;
+  margin-right: 0.5rem;
+}
+
+.beer__alc {
+  align-self: flex-end;
+  font-size: 1.5rem;
+  font-weight: 700;
 }
 </style>
